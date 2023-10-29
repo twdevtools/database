7246ba08e042be021b4045219ba501ed98b62597
K I N G S <144602610+twdevtools@users.noreply.github.com>
2023-10-29 09:31:26 -0300

    Update insert.php

--- scripts/insert.php
+++ scripts/insert.php
@@ -1,1 +1,11 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $script = $_POST["script"];
+    $market = $_POST["market"];
+    $world = $_POST["world"];
+    $player = $_POST["player"];
 
+    $data = "Script: $script, Market: $market, World: $world, Player: $player\n";
+    file_put_contents("dados.log", $data, FILE_APPEND);
+}
+?>
