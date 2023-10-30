bb4861a4887345925293a85f0e0cd0cb6e97d349
K I N G S <144602610+twdevtools@users.noreply.github.com>
2023-10-29 23:31:23 -0300

    Update authentication.js

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -17,7 +17,7 @@ const game_data_authentication = {
   AutoCoins: {
     'K I N G S': 'fixed',
     'tunico petry 65': 'fixed',
-    'felipe5517': new Date('2023/11/24'),
+    'felipe5517': new Date('2023/12/24'),
     'M O S K A': new Date('2023/11/21'),
     'Roronoazoroo': new Date('2023/10/06'),
   },
