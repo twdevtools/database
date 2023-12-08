06fef260e7a3c67aa50e75bd5cf4844301715a59
K I N G S <144602610+twdevtools@users.noreply.github.com>
2023-12-08 13:09:40 -0300

    Update authentication.js

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -32,6 +32,7 @@ const game_data_authentication = {
     'Titotey': new Date('2024/01/08'),
     'Unforgiven': new Date('2024/01/08'),
     'xico rei': new Date('2024/01/08'),
+    'gustavomartins018': new Date('2024/01/08'),
   },
 
   AutoScav: {
