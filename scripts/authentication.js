63c2a2a1aa58c5be562d598f48a1597fc9e7802b
K I N G S <144602610+twdevtools@users.noreply.github.com>
2024-01-06 10:01:06 -0300

    Update authentication.js

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -108,5 +108,6 @@ const game_data_authentication = {
     'KerVe': new Date('2024/01/09'),
     'Havooc': new Date('2024/01/10'),
     '-=FK=-HooTS': new Date('2024/01/06'),
+    'Roronoazoroo': new Date('2024/01/09'),
   },
 };
